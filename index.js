9375f3c3cf90387a06e530b823af2321e8ee239c
Katie Rose <katiefelten@gmail.com>
2019-08-05 17:00:44 -0400

    :rocket: added the remaining endpoints and successfully tested in Insomnia, MVP complete

--- index.js
+++ index.js
@@ -36,5 +36,62 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+// GET /api/users/:id
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "This user ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The user was not found." });
+    });
+});
+
+// DELETE /api/users/:id
+server.delete("/api/users/:id", (req, res) => {
+  Users.remove(req.params.id)
+    .then(count => {
+      if (count && count > 0) {
+        res.status(200).json({
+          message: "The user was deleted."
+        });
+      } else {
+        res.status(404).json({ message: "This user ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The user could not be removed" });
+    });
+});
+
+// PUT /api/users/:id
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res.status(400).json({ message: "Please provide name and bio." });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({
+            message: "This user ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: "The user information could not be modified."
+        });
+      });
+  }
+});
+
 const port = 8000;
 server.listen(port, () => console.log("api running"));
